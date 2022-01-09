7f38cd75fe2caefc0f379846dd041fedc315bb59
ShahSau <shahriarksaurov@gmail.com>
2022-01-09 02:27:25 +0200

    searchbar

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { View, Text,SafeAreaView } from 'react-native'
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
 
-import HeaderTabs from '../components/HeaderTabs'
+import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
-    return (
-        <SafeAreaView>
-            <HeaderTabs />
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      {/* safe area view is for whole phone and vie is just of headerTabs */}
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+    </SafeAreaView>
+  );
 }
-
